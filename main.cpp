c0a13c388dc82ae94297dd993a5c1a46d13af5bb
Santiago Valdez Ulzurrun <51174811+SantiValdezUlzurrun@users.noreply.github.com>
2020-03-13 12:18:01 -0300

    Añadido validación basica

--- main.cpp
+++ main.cpp
@@ -1,19 +1,55 @@
 #include <iostream>
-#include "./Laberinto/Laberinto.h"
 #include "./Pruebas/pruebas.h"
 #include "./Pruebas/Tester.h"
 #include "./Solucionador/Solucionador.h"
 using namespace std;
+#include <stdio.h>
 
+
+bool esLaberintoValido(std::string rutaLaberinto){
+
+	FILE* fp = fopen(rutaLaberinto.c_str(), "rb");
+	bool pudoAbrir = (fp != NULL);
+	if(!pudoAbrir){
+		cout << "No existe el laberinto: " << rutaLaberinto << endl;
+	}
+	return pudoAbrir;
+}
+
+bool esMetodoValido(std::string metodo){
+
+	if(metodo != DIJKSTRA && metodo != DFS &&
+	   metodo != BFS && metodo != A_ESTRELLA){
+
+		cout << "Metodo ingresado invalido..." <<endl;
+		cout << "Metodos validos: " << endl;
+		cout << DIJKSTRA << " " << DFS << " " << BFS << " " << A_ESTRELLA << endl;
+		return false;
+	}
+	return true;
+}
 
 
 int main(int argc, char** argv){
 
-	//std::string metodoDeSolucion = argv[1];
+	/*
+	if(argc != 2){
+		cout << "Faltan comandos..." << endl;
+		cout << "Ejecutar: ./SolucionadorDeLaberintos 'Método' 'Laberinto.bmp' ..." << endl;
+		return 0;
+	}
+	*/
+	//std::string metodo = argv[1];
 	//std::string laberinto = argv[2];
 
 	std::string metodo = "Dijkstra";
 	std::string laberinto = "Laberintote.bmp";
+
+	if(!esLaberintoValido(laberinto) || !esMetodoValido(metodo)){
+
+		return 0;
+	}
+
  	Solucionador * solucionador = new Solucionador(metodo, laberinto);
  	solucionador->solucionar();
  	delete solucionador;
